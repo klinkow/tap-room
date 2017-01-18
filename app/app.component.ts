3b25749e94f3021fd1e959abc356122d050f460c
Bill Klinkow and Erica Nafziger <erica@>
2017-01-18 10:03:30 -0800

    add edit bar (not functional)

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,16 +15,27 @@ import { Component } from '@angular/core';
       <th>Beer's Left</th>
     </thead>
     <tr *ngFor="let currentKeg of kegs">
+      <td><button (click)="editKeg(currentKeg)">Edit</button></td>
       <td>{{currentKeg.name}}</td>
       <td>{{currentKeg.brand}}</td>
       <td>{{currentKeg.style}}</td>
-      <td>\${{currentKeg.price}}</td>
+      <td>\${{currentKeg.price.toFixed(2)}}</td>
       <td>{{currentKeg.abv}}%</td>
       <td>{{currentKeg.description}}</td>
       <td>{{currentKeg.quantity}} pints</td>
       <td></td>
     </tr>
   </table>
+  <div *ngIf="selectedKeg">
+    <input [(ngModel)]="selectedKeg.name">
+    <input [(ngModel)]="selectedKeg.brand">
+    <input [(ngModel)]="selectedKeg.style">
+    <input [(ngModel)]="selectedKeg.price">
+    <input [(ngModel)]="selectedKeg.abv">
+    <input [(ngModel)]="selectedKeg.description">
+    <input [(ngModel)]="selectedKeg.quantity">
+    <button (click)="updateKeg()" type="button">Submit</button>
+  </div>
   `
 })
 
@@ -34,9 +45,22 @@ kegs: Keg[] = [
   new Keg("Total Domination", "Ninkasi", "IPA", 124, 6.7, 5, "A delightful blend of citrus and floral hop notes dominate the senses while a trio of malt adds a clean finish."),
   new Keg("Vanilla Oatis", "Ninkasi", "Oatmeal Stout", 124, 7, 5, "A divine blend of vanilla, coffee, chocolate, and roasted malt notes swirl together to create a smooth Oatmeal Stout with a crisp finish."),
   new Keg("Milk Stout Nitro", "Left Hand Brewing", "Sweet Stout", 124, 6, 5.5, "Dark & delicious, America’s great milk stout will change your perception about what a stout can be.")
-];
+  ];
+
+  selectedKeg = null;
+
+  editKeg(currentKeg) {
+    this.selectedKeg = currentKeg;
+  }
+
+  updateKeg() {
+    this.selectedKeg = null;
+  }
+
 }
 
+
 export class Keg {
+
   constructor(public name: string, public brand: string, public style: string, public quantity: number, public abv: number, public price: number, public description: string) { }
 }
