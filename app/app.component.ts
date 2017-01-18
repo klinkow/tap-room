ad51f3bcb3d3b927501b363c768cd8694a6e405f
Bill Klinkow and Erica Nafziger <bill@>
2017-01-18 09:08:04 -0800

    create initial file setup and 3 kegs to display in table

---
+++ app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Tap Room</h1>
+  <table>
+    <thead>
+      <th>Beer</th>
+      <th>Brand</th>
+      <th>Style</th>
+      <th>Price</th>
+      <th>ABV</th>
+      <th>Description</th>
+      <th>Beer's Left</th>
+    </thead>
+    <tr *ngFor="let currentKeg of kegs">
+      <td>{{currentKeg.name}}</td>
+      <td>{{currentKeg.brand}}</td>
+      <td>{{currentKeg.style}}</td>
+      <td>\${{currentKeg.price}}</td>
+      <td>{{currentKeg.abv}}%</td>
+      <td>{{currentKeg.description}}</td>
+      <td>{{currentKeg.quantity}} pints</td>
+      <td></td>
+    </tr>
+  </table>
+  `
+})
+
+export class AppComponent {
+
+kegs: Keg[] = [
+  new Keg("Total Domination", "Ninkasi", "IPA", 124, 6.7, 5, "A delightful blend of citrus and floral hop notes dominate the senses while a trio of malt adds a clean finish."),
+  new Keg("Vanilla Oatis", "Ninkasi", "Oatmeal Stout", 124, 7, 5, "A divine blend of vanilla, coffee, chocolate, and roasted malt notes swirl together to create a smooth Oatmeal Stout with a crisp finish."),
+  new Keg("Milk Stout Nitro", "Left Hand Brewing", "Sweet Stout", 124, 6, 5.5, "Dark & delicious, America’s great milk stout will change your perception about what a stout can be.")
+];
+}
+
+export class Keg {
+  constructor(public name: string, public brand: string, public style: string, public quantity: number, public abv: number, public price: number, public description: string) { }
+}
