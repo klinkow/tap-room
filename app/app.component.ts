ad697664c822e31de6c0708585a6c42a52b3a98c
Bill Klinkow and Erica Nafziger <bill@>
2017-01-18 16:45:09 -0800

    create different color profiles for beer styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,14 +8,14 @@ import { Component } from '@angular/core';
     <div class="card-container">
       <div class="card" *ngFor="let currentKeg of kegs">
         <div class="card-image waves-effect waves-block waves-light">
-          <div class="fill-level" [style.height.%]="(currentKeg.quantity/124)*100"></div>
+          <div [class]="fillLevel(currentKeg)" [style.height.%]="(currentKeg.quantity/124)*100"></div>
           <img class="activator" src="./resources/img/keg.png">
         </div>
         <div class="card-content">
-          <h2 class="card-title">{{currentKeg.name}}
-          <span class='card-title activator grey-text text-darken-4'><a (click)="editKeg(currentKeg)" class="btn-floating btn-med waves-effect waves-light red right"><i class="material-icons">mode_edit</i></a></span></h2>
+        <span class='card-title activator grey-text text-darken-4'><a (click)="editKeg(currentKeg)" class="btn-floating btn-med waves-effect waves-light red right"><i class="material-icons">mode_edit</i></a></span>
+          <h2 class="card-title">{{currentKeg.name}}</h2>
           <p>{{currentKeg.brand}} | {{currentKeg.style}}</p>
-          <p>\${{currentKeg.price.toFixed(2)}} | {{currentKeg.abv}}%</p>
+          <p>\${{currentKeg.price}} | {{currentKeg.abv}}% ABV</p>
           <p>{{currentKeg.quantity}} pints</p>
           <a (click)="subtractPint(currentKeg)" class="btn center waves-effect waves-light blue">pint</a>
           <a (click)="subtractGrowler(currentKeg)" class="btn center waves-effect waves-light green">growler</a>
@@ -24,15 +24,22 @@ import { Component } from '@angular/core';
           <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
 
           <div *ngIf="selectedKeg">
+            <label>Name</label>
             <input [(ngModel)]="selectedKeg.name">
+            <label>Brand</label>
             <input [(ngModel)]="selectedKeg.brand">
+            <label>Style</label>
             <input [(ngModel)]="selectedKeg.style">
+            <label>Price</label>
             <input [(ngModel)]="selectedKeg.price">
+            <label>ABV</label>
             <input [(ngModel)]="selectedKeg.abv">
+            <label>Description</label>
             <input [(ngModel)]="selectedKeg.description">
+            <label>Quantity</label>
             <input [(ngModel)]="selectedKeg.quantity">
-            <button (click)="updateKeg()" type="button">Submit</button>
           </div>
+
         </div>
       </div><!--end of card-->
     </div><!--end of card-container-->
@@ -44,13 +51,14 @@ import { Component } from '@angular/core';
       <div class="card-image waves-effect waves-block waves-light">
         <img class="activator" src="./resources/img/keg.png">
       </div>
-      <h2>{{currentKeg.name}}</h2>
-      <h5>{{currentKeg.brand}} - {{currentKeg.style}}</h5>
-      <p>\${{currentKeg.price.toFixed(2)}} | {{currentKeg.abv}}%</p>
-      <p>{{currentKeg.quantity}} pints</p>
+      <div class="card-content">
+        <h2 class="card-title">{{currentKeg.name}}</h2>
+        <p>{{currentKeg.brand}} | {{currentKeg.style}}</p>
+        <p>\${{currentKeg.price}} | {{currentKeg.abv}}% ABV</p>
+        <p>{{currentKeg.quantity}} pints</p>
+      </div>
     </div>
   </div>
-
 
   `
 })
@@ -58,12 +66,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   kegs: Keg[] = [
-    new Keg("Total Domination", "Ninkasi", "IPA", 50, 6.7, 5, "A delightful blend of citrus and floral hop notes dominate the senses while a trio of malt adds a clean finish."),
-    new Keg("Vanilla Oatis", "Ninkasi", "Oatmeal Stout", 124, 7, 5, "A divine blend of vanilla, coffee, chocolate, and roasted malt notes swirl together to create a smooth Oatmeal Stout with a crisp finish."),
-    new Keg("Milk Stout Nitro", "Left Hand Brewing", "Sweet Stout", 2, 6, 5.5, "Dark & delicious, America’s great milk stout will change your perception about what a stout can be."),
-    new Keg("Ripstop Rye Pils", "Base Camp", "Rye Pilsner", 124, 5.7, 4.5, "Aromas of European malt and noble-type hops mingle with a dignified, refreshing, and clean malt character that is further distinguished with a generous addition of spicy rye malt, creating a beer that is at once zesty, crisp, hoppy, and incredibly drinkable."),
-    new Keg("Belgian Strong Dark", "Pfriem", "Ale", 124, 10, 6, "This beer has complex flavors of fig dipped in dark chocolate with ripe fruit and toffee."),
-    new Keg("Voodoo Doughnut Grape Guerrilla", "Rogue", "Ale", 124, 7.5, 3, "This ale was inspired by the Voodoo Doughnut Grape Ape doughnut, a creation that features vanilla frosting, grape dust and purple sprinkles.")
+    new Keg("Total Domination", "Ninkasi", "IPA", 1, 6.7, 5, "A delightful blend of citrus and floral hop notes dominate the senses while a trio of malt adds a clean finish."),
+    new Keg("Vanilla Oatis", "Ninkasi", "Stout", 124, 7, 5, "A divine blend of vanilla, coffee, chocolate, and roasted malt notes swirl together to create a smooth Oatmeal Stout with a crisp finish."),
+    new Keg("Milk Stout Nitro", "Left Hand Brewing", "Stout", 2, 6, 5.5, "Dark & delicious, America’s great milk stout will change your perception about what a stout can be."),
+    new Keg("Ripstop Rye Pils", "Base Camp", "Pilsner", 60, 5.7, 4.5, "Aromas of European malt and noble-type hops mingle with a dignified, refreshing, and clean malt character that is further distinguished with a generous addition of spicy rye malt, creating a beer that is at once zesty, crisp, hoppy, and incredibly drinkable."),
+    new Keg("Belgian Strong Dark", "Pfriem", "Ale", 40, 10, 6, "This beer has complex flavors of fig dipped in dark chocolate with ripe fruit and toffee."),
+    new Keg("Voodoo Doughnut Grape Guerrilla", "Rogue", "Ale", 124, 7.5, 3, "This ale was inspired by the Voodoo Doughnut Grape Ape doughnut, a creation that features vanilla frosting, grape dust and purple sprinkles."),
+    new Keg("Hit The Hay", "Temescal", "Saison", 80, 5, 5, "A light saison with notes of coconut.")
   ];
 
   kegsOnOrder : Keg[] = [];
@@ -79,6 +88,18 @@ export class AppComponent {
       alert("this Keg is out of beer!");
     } else {
       alert("this Keg is out of beer!");
+    }
+  }
+
+  fillLevel(keg) {
+    if (keg.quantity < 32) {
+      return "keg-low";
+    } else if (keg.style === "Stout" || keg.style === "Porter") {
+      return "keg-dark";
+    } else if (keg.style === "Red" || keg.style === "Amber" || keg.style === "Saison") {
+      return "keg-red";
+    } else {
+      return "keg-gold";
     }
   }
 
